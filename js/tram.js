c8d93d94ca79ee661a37d7130f692f1db120ea60
jbulger93 <j-bulger@hotmail.co.uk>
2017-07-03 20:34:01 +0100

    tram journey

---
+++ js/tram.js
@@ -0,0 +1,23 @@
+function Tram (route, startingStation) {
+    this._route = route 
+    this._currentStation = startingStation
+}
+    Tram.prototype = {
+        getCurrentStation: function () {
+
+             return this._currentStation
+        },
+        drive: function () {
+            var routeStations = this._route.getStations()
+            var currentStationIndex = routeStations.indexOf(this._currentStation)
+            var nextStationIndex = currentStationIndex + 1
+            
+            if (nextStationIndex >= routeStations.length) {
+                return "end of the line"
+            
+            }
+            this._currentStation = routeStations[nextStationIndex]
+            return "now arriving at : " + this._currentStation.getName() 
+        }
+    
+}
