b39981ad5b560d720332386c99935fc176ff23ab
Anthony Woodcock <anthonywoodcockmusic1991@hotmail.co.uk>
2017-07-03 19:57:29 +0100

    Page 2

---
+++ js/route.js
@@ -0,0 +1,15 @@
+function Route() {
+ this._stations = []
+
+}   
+
+Route.prototype = {
+    addStation: function (station) {
+        this._stations.push(station)
+
+        return
+    },
+    getStations: function (){ 
+        return this._stations
+    }
+}
